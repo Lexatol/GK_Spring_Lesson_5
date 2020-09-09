9a870df269a0d4f9618e4a78810f1aef2d0a3ca6
Alexey Prikhodko <alvlap@mail.ru>
2020-09-09 12:28:25 +0300

    Lesson 5. Home Work

---
+++ src/main/java/geekbrains/controllers/BooksController.java
@@ -0,0 +1,52 @@
+package geekbrains.controllers;
+
+import geekbrains.entities.Book;
+import geekbrains.services.BooksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/books")
+public class BooksController {
+    private BooksService booksService;
+
+    @Autowired
+    public void setBooksService(BooksService booksService) {
+        this.booksService = booksService;
+    }
+
+    @RequestMapping("/list")
+    public String showBooksList(Model model) {
+        List<Book> allBooks = booksService.getAllBooksList();
+        model.addAttribute("BooksList", allBooks);
+        return "books-list";
+    }
+
+    @RequestMapping(path="/add", method=RequestMethod.GET)
+    public String showAddForm(Model model) {
+        Book book = new Book();
+        book.setName_book("Unknown");
+        model.addAttribute("book", book);
+        return "add-book-form";
+    }
+
+    @RequestMapping(path="/add", method=RequestMethod.POST)
+    public String showAddForm(Book book) {
+        booksService.addBook(book);
+        return "redirect:/books/list";
+    }
+
+    @RequestMapping(path="/remove/{id}", method=RequestMethod.GET)
+    public String removeById(@PathVariable(value = "id") Long bookId) {
+        booksService.removeById(bookId);
+        return "redirect:/books/list";
+    }
+
+
+}
